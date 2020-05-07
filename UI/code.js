55d64dab7208adc27477d464447f5f130f137818
Samarth Hiremath <samarthhiremath@Samarths-MacBook-Pro.local>
2020-05-06 22:18:35 -0400

    Removed comment

--- UI/code.js
+++ UI/code.js
@@ -96,7 +96,6 @@ function previewFile(input) {
 }
 
 function uploadImage(event){
-  // Uploading images
   console.log("Encoded",encoded);
   var files = document.getElementById("imgfile").files;
 
